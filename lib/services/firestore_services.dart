48ec2ca1c3ff54803ab2c715f762982adfd84be9
bobycat22 <anthonyznj@gmail.com>
2022-02-21 00:13:54 -0500

    commit before adding waiting list functionality

--- lib/services/firestore_services.dart
+++ lib/services/firestore_services.dart
@@ -226,6 +226,215 @@ class FirestoreServices {
   }
 
   // ************ PEER SERVICES ***************** //
+
+  // ==WORK IN PROGRESS==
+
+  Future<String> requestPeer(String currentUserId) async {
+    var users = await FirebaseFirestore.instance.collection('Users');
+    List<String> userIds = new List.from([]);
+    List<String> allIds = new List.from([]);
+    List<String> acceptingIds = new List.from([]);
+    var userDocument = await FirebaseFirestore.instance
+        .collection('Users')
+        .doc(currentUserId.trim())
+        .get();
+    Map<String, dynamic>? userData = userDocument.data();
+    String lastRequestedAt = userData?['lastRequestedAt'];
+    int totalRequests = userData?['totalRequests'];
+    List<dynamic> volunteersChattedWith = userData?['specialChattedWith'];
+    String school = userData?['school'];
+    List<dynamic> blocked = userData?['blocked'];
+    List<dynamic> requestedIds = userData?['requestedIds'];
+    var status;
+    if (lastRequestedAt != '') {
+      DateTime lastReportedTimeAt =
+          DateTime.fromMillisecondsSinceEpoch(int.parse(lastRequestedAt));
+      DateTime currentDateTime = DateTime.now();
+      var difference =
+          currentDateTime.difference(lastReportedTimeAt).inMilliseconds;
+      // it's been less than an 1 hour, has requested % 3 times
+      // if (difference <= 3600000 || totalRequests % 3 == 0) {
+      if (difference <= 3600000 && totalRequests % 3 == 0) {
+        return 'Volunteer Request cool down (1 hour)';
+      }
+    }
+    await users
+        // status - active?
+        // .where('role', isEqualTo: 'Chat Buddy')
+        .get()
+        .then((QuerySnapshot snapshot) async {
+      snapshot.docs.forEach((DocumentSnapshot doc) {
+        allIds.add(doc.id);
+        String id = doc.id;
+        bool isAccepting = doc.get('isAccepting');
+        if (isAccepting) {
+          acceptingIds.add(id);
+        }
+        if (id != currentUserId &&
+            !volunteersChattedWith.contains(id) &&
+            !blocked.contains(id) &&
+            !requestedIds.contains(id) &&
+            isAccepting) {
+          userIds.add(id);
+        }
+      });
+
+      Random rng = new Random();
+      String randomId;
+      int randomNum;
+      bool keepHistory;
+      bool addHistory;
+
+      if (userIds.isEmpty) {
+        // will disregard history
+        if (acceptingIds.isEmpty) {
+          randomNum = rng.nextInt(allIds.length);
+          randomId = allIds[randomNum];
+          keepHistory = false;
+          addHistory = false;
+        } else {
+          randomNum = rng.nextInt(acceptingIds.length);
+          randomId = acceptingIds[randomNum];
+          keepHistory = true;
+          addHistory = false;
+        }
+      } else {
+        randomNum = rng.nextInt(userIds.length);
+        randomId = userIds[randomNum];
+        keepHistory = true;
+        addHistory = true;
+      }
+
+      var requests = await FirebaseFirestore.instance.collection('Requests');
+
+      String timestamp = DateTime.now().millisecondsSinceEpoch.toString();
+
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      String? photoUrl = await prefs.getString('photoUrl');
+      String? currentUserName = await prefs.getString('alias');
+
+      requests.add({
+        'peer': currentUserId,
+        'peerPhotoUrl': photoUrl,
+        'volunteer': randomId,
+        'peerName': currentUserName,
+        'timestamp': timestamp,
+        'keepHistory': keepHistory,
+        'addHistory': addHistory,
+      }).then((doc) async {
+        await FirebaseFirestore.instance
+            .collection('Users')
+            .doc(currentUserId)
+            .update({
+          'lastRequestedAt': timestamp,
+          'totalRequests': FieldValue.increment(1),
+          'requestedIds': FieldValue.arrayUnion([randomId]),
+        });
+        status = 'Success';
+        return 'Success';
+      }).catchError((err) {
+        print('Error processing request: $err');
+        status = err.toString();
+        return err.toString();
+      });
+      status = 'Success';
+    }).catchError((err) {
+      print('Error processing request: $err');
+      status = err.toString();
+      return err;
+    });
+    return status;
+  }
+
+  Future<String> grantPeerRequest(String userId, String requestId,
+      String peerId, bool keepHistory, bool addHistory) async {
+    var status;
+
+    var peerUserDocument = await FirebaseFirestore.instance
+        .collection('Users')
+        .doc(peerId.trim())
+        .get();
+    Map<String, dynamic>? peerUserData = peerUserDocument.data();
+    String peerName = peerUserData!['alias'];
+    String peerPhotoUrl = peerUserData['photoUrl'];
+
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? alias = await prefs.getString('alias');
+    String? photoUrl = await prefs.getString('photoUrl');
+
+    var groups = await FirebaseFirestore.instance.collection('Groups');
+
+    String timestamp = DateTime.now().millisecondsSinceEpoch.toString();
+
+    groups.add({
+      'members': [peerId, userId],
+      'memberNames': [peerName, alias],
+      'memberPhotoUrls': [peerPhotoUrl, photoUrl],
+      'lastMessage': 'New Conversation - Hi!',
+      'lastTimestamp': timestamp,
+      'lastUpdatedBy': userId,
+      'createdAt': timestamp,
+      'createdBy': peerId,
+      'type': 'Peer-Volunteer',
+    }).then((doc) async {
+      String groupId = doc.id;
+
+      if (addHistory) {
+        // keep history if still users they havent talked to
+        FirebaseFirestore.instance.collection('Users').doc(peerId).update({
+          'groups': FieldValue.arrayUnion([groupId]),
+          'specialChattedWith':
+              keepHistory ? FieldValue.arrayUnion([userId]) : [userId],
+          // 'lastConnected': timestamp,
+          'requestedIds': FieldValue.arrayRemove([userId]),
+        });
+        FirebaseFirestore.instance.collection('Users').doc(userId).update({
+          'groups': FieldValue.arrayUnion([groupId]),
+          'specialChattedWith': FieldValue.arrayUnion([peerId]),
+          'lastConnected': timestamp,
+        });
+      } else {
+        FirebaseFirestore.instance.collection('Users').doc(peerId).update({
+          'groups': FieldValue.arrayUnion([groupId]),
+          // 'lastConnected': timestamp,
+          'requestedIds': FieldValue.arrayRemove([userId]),
+        });
+        FirebaseFirestore.instance.collection('Users').doc(userId).update({
+          'groups': FieldValue.arrayUnion([groupId]),
+          // doesn't really matter for the volunteer
+          // 'specialChattedWith': FieldValue.arrayUnion([peerId]),
+          'lastConnected': timestamp,
+        });
+      }
+
+      await FirebaseFirestore.instance
+          .collection('Requests')
+          .doc(requestId)
+          .delete();
+
+      status = 'Success';
+
+      return 'Success';
+    }).catchError((err) {
+      print('Error creating group: $err');
+      status = err;
+      return err;
+    });
+    status = 'Success';
+
+    return status;
+  }
+
+  Future<String> declineRequest(String requestId) async {
+    await FirebaseFirestore.instance
+        .collection('Requests')
+        .doc(requestId)
+        .delete();
+    return 'Success';
+  }
+
+  // =====================
+
   Future<String> createGroup(String currentUserId) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     String? alias = await prefs.getString('alias');
